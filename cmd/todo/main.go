cdacde0dad827ea3b9249f1179678b6def3f174d
jaysonesmith <gh@nes.33mail.com>
2021-10-10 21:39:53 -0700

    Add getTask

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/mxygem/todo"
 )
@@ -19,7 +22,7 @@ func main() {
 		flag.PrintDefaults()
 	}
 
-	task := flag.String("task", "", "task to be included in the todo list")
+	add := flag.Bool("add", false, "add a task to a todo list")
 	list := flag.Bool("list", false, "list all tasks")
 	complete := flag.Int("complete", 0, "item to be completed")
 	flag.Parse()
@@ -47,8 +50,13 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-	case *task != "":
-		l.Add(*task)
+	case *add:
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(t)
 
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
@@ -58,4 +66,21 @@ func main() {
 		fmt.Fprintln(os.Stderr, "invalid option")
 		os.Exit(1)
 	}
+}
+
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("task cannot be blank")
+	}
+
+	return s.Text(), nil
 }
